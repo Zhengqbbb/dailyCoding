b03ce498084100b27cb25266e4ea52f54bba82fb
zhengqbbb <1074059947@qq.com>
2019-08-15 23:47:27 +0800

    updata

---
+++ JavaScript/7.BindRealization.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: qbenben 
+ * @Date: 2019-08-15 23:44:48 
+ * @Last Modified by: qbenben
+ * @Last Modified time: 2019-08-15 23:45:09
+ * 手写bind
+ */
---
+++ JavaScript/8.PromiseRealization.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: qbenben 
+ * @Date: 2019-08-15 23:44:50 
+ * @Last Modified by: qbenben
+ * @Last Modified time: 2019-08-15 23:45:18
+ * 手写Promise
+ */
