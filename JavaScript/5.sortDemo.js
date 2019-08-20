d773de71ef39f65055cbfad5cca93a7f910046f3
zhengqbbb <1074059947@qq.com>
2019-08-20 21:33:21 +0800

    updata

--- JavaScript/5.sortDemo.js
+++ JavaScript/5.sortDemo.js
@@ -2,7 +2,7 @@
  * @Author: qbenben 
  * @Date: 2019-08-12 04:16:24 
  * @Last Modified by: qbenben
- * @Last Modified time: 2019-08-18 00:58:08
+ * @Last Modified time: 2019-08-20 21:32:57
  * 排序算法练习
  */
 
@@ -120,6 +120,40 @@ function binaryInsertionSort(arr) {
   }
   return arr;
 }
+
+
+//--------------------希尔排序---------------------
+/**
+ * <1>. 选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；
+ * <2>.按增量序列个数k，对序列进行k 趟排序；
+ * <3>.每趟排序，根据对应的增量ti，将待排序列分割成若干长度为m 的子序列，分别对各子表进行直接插入排序。仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+ * 最佳情况：T(n) = O(nlog2 n)
+ * 最坏情况：T(n) = O(nlog2 n)
+ * 平均情况：T(n) =O(nlog n)
+ * @param  {} arr
+ */
+var shellSort = function(arr) {
+  var len = arr.length,
+    temp,
+    gap = 1;
+  console.time('希尔排序耗时:');
+  while (gap < len / 5) { //动态定义间隔序列
+    gap = gap * 5 + 1;
+  }
+  for (gap; gap > 0; gap = Math.floor(gap / 5)) {
+    for (var i = gap; i < len; i++) {
+      temp = arr[i];
+      for (var j = i - gap; j >= 0 && arr[j] > temp; j -= gap) {
+        arr[j + gap] = arr[j];
+      }
+      arr[j + gap] = temp;
+    }
+  }
+  console.timeEnd('希尔排序耗时:');
+  return arr;
+}
+
+
 
 
 //--------------快速排序-------------------
