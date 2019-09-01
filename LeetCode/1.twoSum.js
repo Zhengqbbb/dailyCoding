d0c476e2a28323fc83bd41e5944cfb4ceb8ea305
zhengqbbb <1074059947@qq.com>
2019-09-01 15:54:27 +0800

    updata

--- LeetCode/1.twoSum.js
+++ LeetCode/1.twoSum.js
@@ -2,13 +2,13 @@
  * @Author: qbenben 
  * @Date: 2019-08-29 21:08:06 
  * @Last Modified by: qbenben
- * @Last Modified time: 2019-08-31 21:25:07
+ * @Last Modified time: 2019-09-01 15:42:00
  * Leetcode001————两数之和(two-sum)
  */
 // 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
-var testNums = [2, 7, 11, 15];
-var tagter = 9;
+var testNums = [1, 3, 2, 5, 6];
+var tagter = 8;
 
 
 
@@ -31,10 +31,13 @@ var twoSum_1 = function(nums, targer) {
 
 
 /**
+ * 1.开辟一块内存空间 result 
+ * 2.通过map遍历nums，并使用indexOf寻找当前item的index之外和item相加等于target的结果
+ * 3.最后把结果sort排序
  * @param   nums  数组
  * @param  targer 目标值
  */
-var twoSum_2 = function(nums, target) {
+/* var twoSum_2 = function(nums, target) {
   let result = [];
   nums.map((item, index) => {
     if (nums.indexOf(target - item) > -1 && nums.indexOf(target - item) != index) {
@@ -42,10 +45,25 @@ var twoSum_2 = function(nums, target) {
     }
   });
   return result;
-};
+}; */
+
+var twoSum_2 = function(nums, target) {
+  let result = [];
+  nums.map((item, index) => {
+    if (nums.indexOf(target - item) !== -1 && nums.indexOf(target - item) !== index) {
+      result = [index, nums.indexOf(target - item)].sort((a, b) => a > b);
+    }
+  });
+  return result;
+}
+
+//console.log(twoSum_2(testNums, 8));
 
 
 /**
+ * 使用Map对象
+ * 判断nums[i]是否存在于Map对象中，没有的话，就存入target - nums[i]到Map
+ * 返回[map.get(nums[i], i)]
  * @param   nums  数组
  * @param  targer 目标值
  */
@@ -59,3 +77,5 @@ var twoSum_3 = function(nums, target) {
     }
   }
 };
+
+//console.log(twoSum_3(testNums, 8));
