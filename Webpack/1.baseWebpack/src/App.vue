7380f8e7c596164f19ac649626738ed0d43ab71e
zhengqbbb <1074059947@qq.com>
2019-10-03 23:56:45 +0800

    :sparkles: Done to 1.baseWebpack

--- Webpack/1.baseWebpack/src/App.vue
+++ Webpack/1.baseWebpack/src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
     hello vue
+    
   </div>
 </template>
+
+<script lang="ts">
+import {Component,Vue} from 'vue-property-decorator';
+
+@Component({
+  components:{
+    //TODO...
+  }
+})
+export default class Todo extends Vue({
+  public todos:string[] = ['香蕉', '苹果'];
+})
+</script>
