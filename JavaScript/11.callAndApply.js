c0bbb6fa1fb15fe0f0274afa89e0f9978657a4fe
zhengqbbb <1074059947@qq.com>
2020-03-31 20:42:45 +0800

    :construction: Add 11.call and apply

---
+++ JavaScript/11.callAndApply.js
@@ -0,0 +1,40 @@
+/*
+ * @Author: Qbenben
+ * @Date: 2020-03-31 20:30:21
+ * @LastEditors: Qbenben
+ * @LastEditTime: 2020-03-31 20:42:14
+ * @Description: call 和 apply的实现
+ */
+
+
+
+Function.prototype.call2 = function(context) {
+  var context = context || window;
+  context.fn = this;
+  var arr = [];
+  for (let i = 1, len = arguments.length; i < len; i++) {
+    arr.push('arguments[' + i + ']');
+  }
+  var result = eval('context.fn(' + arr + ')');
+  delete context.fn;
+  return result;
+}
+
+
+Function.prototype.apply2 = function(context, arr) {
+  var context = Object(context) || window;
+  context.fn = this;
+
+  var result;
+  if (!arr) {
+    result = context.fn;
+  } else {
+    var args = [];
+    for (i = 0, len = arr.length; i < len; i++) {
+      args.push('arguments[' + i + ']');
+    }
+    result = eval('context.fn(' + args + ')')
+  }
+  delete context.fn;
+  return result;
+}
