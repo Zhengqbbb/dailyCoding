0b2dfe0fb5974640e4e9c136b345f7941320a577
zhengqbbb <1074059947@qq.com>
2020-05-19 21:16:49 +0800

    :construction:Add comment

--- JavaScript/9.NewRealization.js
+++ JavaScript/9.NewRealization.js
@@ -2,7 +2,7 @@
  * @Author: Qbenben
  * @Date: 2020-03-22 02:06:03
  * @LastEditors: Qbenben
- * @LastEditTime: 2020-03-22 02:10:50
+ * @LastEditTime: 2020-03-31 21:27:02
  * @Description: New 的实现
  */
 
@@ -13,7 +13,7 @@ function _new() {
   let Con = [].shift.call(arguments);
   //原型链绑定
   obj.__proto__ = Con.prototype;
-  //构造函数的执行
+  //构造函数的执行同时借用外部传入的构造器给obj设置属性
   let result = Con.apply(obj, arguments);
   //判断new出来的是不是一个对象
   return typeof result === 'object' ? result : obj;
