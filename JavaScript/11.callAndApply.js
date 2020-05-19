ac4400d78346716e49d8b1ce733d721963087ab4
zhengqbbb <1074059947@qq.com>
2020-05-19 21:16:00 +0800

    :construction: Add 11.callAndApply

--- JavaScript/11.callAndApply.js
+++ JavaScript/11.callAndApply.js
@@ -2,39 +2,39 @@
  * @Author: Qbenben
  * @Date: 2020-03-31 20:30:21
  * @LastEditors: Qbenben
- * @LastEditTime: 2020-03-31 20:42:14
+ * @LastEditTime: 2020-05-14 23:33:51
  * @Description: call 和 apply的实现
  */
 
 
 
 Function.prototype.call2 = function(context) {
-  var context = context || window;
-  context.fn = this;
-  var arr = [];
-  for (let i = 1, len = arguments.length; i < len; i++) {
-    arr.push('arguments[' + i + ']');
-  }
-  var result = eval('context.fn(' + arr + ')');
-  delete context.fn;
-  return result;
+    var context = context || window;
+    context.fn = this;
+    var arr = [];
+    for (let i = 1, len = arguments.length; i < len; i++) {
+        arr.push('arguments[' + i + ']');
+    }
+    var result = eval('context.fn(' + arr + ')');
+    delete context.fn;
+    return result;
 }
 
 
 Function.prototype.apply2 = function(context, arr) {
-  var context = Object(context) || window;
-  context.fn = this;
+    var context = Object(context) || window;
+    context.fn = this;
 
-  var result;
-  if (!arr) {
-    result = context.fn;
-  } else {
-    var args = [];
-    for (i = 0, len = arr.length; i < len; i++) {
-      args.push('arguments[' + i + ']');
+    var result;
+    if (!arr) {
+        result = context.fn;
+    } else {
+        var args = [];
+        for (i = 0, len = arr.length; i < len; i++) {
+            args.push('arguments[' + i + ']');
+        }
+        result = eval('context.fn(' + args + ')')
     }
-    result = eval('context.fn(' + args + ')')
-  }
-  delete context.fn;
-  return result;
+    delete context.fn;
+    return result;
 }
